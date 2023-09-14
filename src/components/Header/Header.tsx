58f14fff1b5c5a1410050dd3631aa8457091bbf3
“kkoisland” <47127802+kkoisland@users.noreply.github.com>
2023-09-13 21:38:41 -0700

    wip Header on 2023/9/13

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -27,21 +27,29 @@ const Header = () => {
 
   return (
     <header>
-      <div onClick={() => navigate('/')}>KKoisland Header</div>
-      <div style={{ textAlign: 'right', padding: '0 10px' }}>
-        <a href="https://github.com/kkoisland" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon className="iconStyle" icon={faGithub} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/keiko-higuchi-b554449/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon className="iconStyle" icon={faLinkedin} size="2x" />
-        </a>
-        <a href="https://twitter.com/kkoisland" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon className="iconStyle" icon={faTwitter} />
-        </a>
+      <div className="header-top">
+        <div onClick={() => navigate('/')}>KKoisland Header</div>
+        <div className="icon-style-div">
+          <a href="https://github.com/kkoisland" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon className="iconStyle" icon={faGithub} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/keiko-higuchi-b554449/"
+            target="_blank"
+            // className="iconStyle"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon className="iconStyle" icon={faLinkedin} size="2x" />
+          </a>
+          <a
+            href="https://twitter.com/kkoisland"
+            target="_blank"
+            // className="iconStyle"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon className="iconStyle" icon={faTwitter} />
+          </a>
+        </div>
       </div>
       <nav>
         <ul className="year-list-item">
