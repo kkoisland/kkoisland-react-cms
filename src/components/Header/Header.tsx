adaaadd9877c311b5aa3caddaccf8e7ceac84696
“kkoisland” <47127802+kkoisland@users.noreply.github.com>
2023-09-14 08:17:58 -0700

    Header css on 2023/9/13

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -29,24 +29,18 @@ const Header = () => {
     <header>
       <div className="header-top">
         <div onClick={() => navigate('/')}>KKoisland Header</div>
-        <div className="icon-style-div">
+        <div className="icon-div">
           <a href="https://github.com/kkoisland" target="_blank" rel="noreferrer">
             <FontAwesomeIcon className="iconStyle" icon={faGithub} />
           </a>
           <a
             href="https://www.linkedin.com/in/keiko-higuchi-b554449/"
             target="_blank"
-            // className="iconStyle"
             rel="noreferrer"
           >
             <FontAwesomeIcon className="iconStyle" icon={faLinkedin} size="2x" />
           </a>
-          <a
-            href="https://twitter.com/kkoisland"
-            target="_blank"
-            // className="iconStyle"
-            rel="noreferrer"
-          >
+          <a href="https://twitter.com/kkoisland" target="_blank" rel="noreferrer">
             <FontAwesomeIcon className="iconStyle" icon={faTwitter} />
           </a>
         </div>
