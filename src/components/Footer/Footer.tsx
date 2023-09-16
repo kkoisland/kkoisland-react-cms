9daf71744de8d973d75e4d045784e8748838c772
“kkoisland” <47127802+kkoisland@users.noreply.github.com>
2023-09-16 15:19:52 -0700

    Header/Footer style on 2023/9/16

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,2 +1,38 @@
-const Footer = () => <footer>This is Footer</footer>;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>
+        <a
+          href="https://github.com/kkoisland"
+          className="icon-style"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} size="1x" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/keiko-higuchi-b554449/"
+          target="_blank"
+          className="icon-style"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="1x" />
+        </a>
+        <a
+          href="https://twitter.com/kkoisland"
+          target="_blank"
+          className="icon-style"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitter} size="1x" />
+        </a>
+      </div>
+      <div>© 2018–2023 kkoisland.com</div>
+    </footer>
+  );
+};
 export default Footer;
