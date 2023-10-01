8284ded794026f3fc1466c25b044cfcfdcefa709
“kkoisland” <47127802+kkoisland@users.noreply.github.com>
2023-10-01 14:59:19 -0700

    07extractImage.pl succeed to extract year/image.jpg for 1 file on 2023/10/1

--- scripts/07extractImage.pl
+++ scripts/07extractImage.pl
@@ -14,19 +14,28 @@ my $output_file = 'blogData_extractImage.json';
 open(my $json_input, '<:encoding(UTF-8)', $input_file) or die "Cannot open $input_file: $!";
 my $json_data = join('', <$json_input>);
 close($json_input);
-###
-# ファイル検索と画像取得
-# 入力のHTMLテキスト（例として一部を示します）
-my $html = '... <!-- main blog entry column -->
-<img src="/bimages/2018/062.jpg" width="800" height="533" border="0"><br>
-<img src="/bimages/2020/123.jpg" width="800" height="533" border="0"><br>
-...';
 
+###ここから、書き直してください
+# 読み込むHTMLファイルのパス
+my $read_file = 'blog/2018/12/01/index.html';
 
-# "image": "noimage" を "image": "2018/066.jp" に置換
-$json_data =~ s/"image": "noimage"/"image": "2018\/066.jpg"/g;
+my $new_image;
 
-###
+# ファイルを一行ずつ読み込み、条件に合致する行を置換
+open(my $html_file, '<:encoding(UTF-8)', $read_file) or die "Cannot open $read_file: $!";
+while (my $line = <$html_file>) {
+    if ($line =~ /<img src="\/bimages\/(\d{4}\/\d{3}\.jpg)"/) {
+        $new_image = $1;
+        last; # マッチしたらループを抜ける
+    }
+}
+close($html_file);
+
+# $new_image が設定されている場合のみ置換
+if (defined $new_image) {
+    $json_data =~ s/"image": "noimage"/"image": "$new_image"/;
+}
+###ここまで、書き直してください
 
 # アウトプットファイルに書き出す
 open(my $json_output, '>:encoding(UTF-8)', $output_file) or die "Cannot open $output_file: $!";
